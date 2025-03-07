10d92510697bd8d58a47565b62174e72e59d37bf
sangram <balsangram1@gmail.com>
2025-03-08 00:33:19 +0530

    DEL : clean css file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name , setName] = useState('');
+  const [email, setEmail] = useState('');
+  function handelName(e) {
+    console.log(e,"eee");
+    
+    // const {name , value} = e.target
 
+    setName(e.target.value)
+    // setEmail(e.target.value.email)
+    console.log(name,"name");
+    // console.log(email,"email");
+  }
+  function handelEmail(e) {
+    console.log(e,"eee");
+    
+    // const {name , value} = e.target
+
+    // setName(e.target.value)
+    setEmail(e.target.value)
+    console.log(email,"email");
+    // console.log(email,"email");
+  }
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <label htmlFor="name">
+        Name : <input type="text" 
+        value={name}
+        onChange={handelName}
+        />
+      </label>
+      <label htmlFor="email">
+        Email : <input type="text" 
+        value={email}
+        onChange={handelEmail}
+        />
+      </label>
+      <p>name is {name} and email is {email}</p>
+    </div>
   )
 }
 
